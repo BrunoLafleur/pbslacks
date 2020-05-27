f84c69ee5291b3edb31a003c67a57684c0303acf
Pierre Brochard <pierre@famillebrochard.fr>
2020-05-27 13:22:34 +0200

    pbslacks V0.22 Date : 25 mai 2020 Auteur : Pierre Brochard (pierre.brochard.1982@m4x.org) Copyright et droits d'auteurs : Pierre Brochard

--- windows/setnormalmode.bat
+++ windows/setnormalmode.bat
@@ -2,14 +2,14 @@
 REM Auteur : Pierre Brochard (pierre.brochard.1982@m4x.org)
 REM Date : 2019
 REM License : See the LICENSE file in the top repository.
-echo Remet Windows en mode normal au prochain redémarrage s'il était en mode sans échec.
+echo Remet Windows en mode normal au prochain redemarrage s'il etait en mode sans echec.
 echo Vous devez etre en mode administrateur.
 echo -------------------------------------------------------------------------
-echo Utile une fois les drivers SATA AHCI chargés en mode sans échec pour repasser en mode normal.
+echo Utile une fois les drivers SATA AHCI charges en mode sans echec pour repasser en mode normal.
 echo -------------------------------------------------------------------------
 
 bcdedit /deletevalue {current} safeboot
-echo Vous pouvez maintenant redémarrer la machine et aller dans le BIOS
+echo Vous pouvez maintenant redemarrer la machine et aller dans le BIOS
 echo pour le mettre en mode SATA AHCI.
 echo La commande setnormalmode permettra ensuite de remettre le mode normal.
 pause
