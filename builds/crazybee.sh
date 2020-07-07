fb9cc237cc175f1659e1a09940e82fc6ea4127c3
Pierre Brochard <pierre@famillebrochard.fr>
2020-07-07 23:29:14 +0200

    pbslacks V0.25 Date : 23 june 2020 Auteur : Pierre Brochard (pierre.brochard.1982@m4x.org) Copyright et droits d'auteurs : Pierre Brochard

--- builds/crazybee.sh
+++ builds/crazybee.sh
@@ -10,66 +10,62 @@ REINSTALL=${REINSTALL:-no}
 UPDATE=${UPDATE:-yes}
 
 if [ ! $UID = 0 ]; then
-  cat << EOF
+    cat << EOF
 This script must be run as root.
 EOF
-  exit 1
+    exit 1
 fi
 
 if [ -e ~/Bumblebee-SlackBuilds/crazybee.sh ]; then
-  echo "Reinstall!"
-  export REINSTALL=${REINSTALL:-yes}
+    echo "Reinstall!"
+    export REINSTALL=${REINSTALL:-yes}
 elif [ -d ~/Bumblebee-SlackBuilds/ ]; then
-  cat << EOF
+    cat << EOF
 You already have the bumblebee directory,
 please rename to avoid changes you may have made.
 EOF
-  exit 1
+    exit 1
 fi
 
 if [ "$(uname -m)" = "x86_64" ] && [ -e /lib/libc.so.6 ]; then
-  export COMPAT32=yes;
+    export COMPAT32=yes;
 fi
 
 install_latest_pkg() {
-  PACKAGE=$1
-  if [ -d "$PACKAGE/" ]; then
-    cd $PACKAGE/
-  else
-    cd ../$PACKAGE/
-  fi
-  sh $PACKAGE.SlackBuild
-  ls -t --color=never /tmp/$PACKAGE-*_bbsb.txz | head -1 | xargs -i upgradepkg --reinstall --install-new {}
+    PACKAGE=$1
+    (
+	[ -d "$PACKAGE/" ] && cd $PACKAGE
+	sh $PACKAGE.SlackBuild
+	ls -t --color=never /tmp/$PACKAGE-*_bbsb.txz | head -1 | xargs -i upgradepkg --reinstall --install-new {}
+    )
 }
 
 install_latest_pkg_compat() {
-  PACKAGE=$1
-  if [ -d "$PACKAGE/" ]; then
-    cd $PACKAGE/
-  else
-    cd ../$PACKAGE/
-  fi
-  if [ "$COMPAT32" = yes ]; then
-    COMPAT32=yes sh $PACKAGE.SlackBuild
-  else
-    sh $PACKAGE.SlackBuild
-  fi
-  ls -t --color=never /tmp/$PACKAGE-*_bbsb.txz | head -1 | xargs -i upgradepkg --reinstall --install-new {}
+    PACKAGE=$1
+    (
+	[ -d "$PACKAGE/" ] && cd $PACKAGE
+	if [ "$COMPAT32" = yes ]; then
+	    COMPAT32=yes sh $PACKAGE.SlackBuild
+	else
+	    sh $PACKAGE.SlackBuild
+	fi
+	ls -t --color=never /tmp/$PACKAGE-*_bbsb.txz | head -1 | xargs -i upgradepkg --reinstall --install-new {}
+    )
 }
 
 if [ "$UPDATE" = "yes" -a "$REINSTALL" = "no" ]; then
-  cd
-  git clone https://github.com/WhiteWolf1776/Bumblebee-SlackBuilds.git
-  cd Bumblebee-SlackBuilds/
+    cd
+    git clone https://github.com/WhiteWolf1776/Bumblebee-SlackBuilds.git
+    cd Bumblebee-SlackBuilds/
 fi
 
 if [ "$UPDATE" = "yes" -a "$STABLE" = "yes" ]; then
-  git checkout 14.1
+    git checkout 14.1
 fi
 
 ## in case there are updates ;-)
 if [ "$UPDATE" = "yes" -a "$REINSTALL" = "yes" ]; then
-  git pull
+    git pull
 fi
 
 ## let's get some tarballs!
@@ -77,7 +73,8 @@ sh download.sh
 
 groupadd bumblebee
 ## add all non-root users (except ftp) to bumblebee group
-getent passwd | grep "/home" | cut -d: -f1 | sed '/ftp/d' | xargs -i usermod -G bumblebee -a {}
+getent passwd | grep "/home" | cut -d: -f1 | sed '/ftp/d' | \
+    xargs -i usermod -G bumblebee -a {}
 
 install_latest_pkg libbsd
 
@@ -88,19 +85,22 @@ install_latest_pkg bbswitch
 install_latest_pkg_compat primus
 install_latest_pkg_compat primus_vk
 
-cd ../nouveau-blacklist/
-removepkg xf86-video-nouveau-blacklist-noarch-1.txz
-ls -t --color=never xf86-video-nouveau-blacklist-*noarch-[0-9]*.txz | \
-    head -1 | xargs -i upgradepkg --install-new --reinstall {}
+(
+    cd nouveau-blacklist/
+    removepkg xf86-video-nouveau-blacklist-noarch-1.txz
+    ls -t --color=never xf86-video-nouveau-blacklist-*noarch-[0-9]*.txz | \
+	head -1 | xargs -i upgradepkg --install-new --reinstall {}
+)
+
 if [ -z "$(grep xf86-video-nouveau /etc/slackpkg/blacklist)" ]; then
-  echo xf86-video-nouveau >> /etc/slackpkg/blacklist
+    echo xf86-video-nouveau >> /etc/slackpkg/blacklist
 fi
 if [ -z "$(grep _bbsb /etc/slackpkg/blacklist)" ]; then
-  echo "[0-9]+_bbsb" >> /etc/slackpkg/blacklist
+    echo "[0-9]+_bbsb" >> /etc/slackpkg/blacklist
 fi
 
 if [ "$STABLE" = "yes" ]; then
-  install_latest_pkg libvdpau
+    install_latest_pkg libvdpau
 fi
 
 install_latest_pkg nvidia-kernel
@@ -111,14 +111,14 @@ chmod +x /etc/rc.d/rc.bumblebeed
 /etc/rc.d/rc.bumblebeed start
 
 if [ -z "$(grep bumblebeed /etc/rc.d/rc.local)" ]; then
-echo "if [ -x /etc/rc.d/rc.bumblebeed ]; then
-  /etc/rc.d/rc.bumblebeed start
+    echo "if [ -x /etc/rc.d/rc.bumblebeed ]; then
+    /etc/rc.d/rc.bumblebeed start
 fi" >> /etc/rc.d/rc.local
 fi
 
 if [ -z "$(grep bumblebeed /etc/rc.d/rc.local_shutdown)" ]; then
-echo "if [ -x /etc/rc.d/rc.bumblebeed ]; then
-  /etc/rc.d/rc.bumblebeed stop
+    echo "if [ -x /etc/rc.d/rc.bumblebeed ]; then
+    /etc/rc.d/rc.bumblebeed stop
 fi" >> /etc/rc.d/rc.local_shutdown
 fi
 
@@ -127,7 +127,7 @@ chmod 755 /etc/rc.d/rc.local
 chmod 755 /etc/rc.d/rc.local_shutdown
 
 if [ "$UPDATE" = "yes" ]; then
-  wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/crazybee.sh -P ~/Bumblebee-SlackBuilds
+    wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/crazybee.sh -P ~/Bumblebee-SlackBuilds
 fi
 
 echo
