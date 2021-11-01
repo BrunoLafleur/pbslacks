61d732aa5b8db3d2715e29abb6f089bd9d2dbe60
Pierre Brochard <pierre@famillebrochard.fr>
2021-11-01 13:52:40 +0100

    pbslacks V0.69-20211030 Date : 30 octobre 2021 Auteur : Pierre Brochard (pierre.brochard.1982@m4x.org) Copyright et droits d'auteurs : Pierre Brochard

---
+++ LocaleFR/install.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+cp lang.* pbalias.sh pbgnomekeyring.sh /etc/profile.d
+cp csh.cshrc /etc
+
+if [ `ls /usr/share/X11/xorg.conf.d/90-* 2> /dev/null|wc -l` -eq 0 ] ;then
+  cp /usr/share/X11/xorg.conf.d/90-* /etc/X11/xorg.conf.d
+  echo "Changez clavier dans `ls /etc/X11/xorg.conf.d/90-*`"
+fi
+
+echo 'Pour gnome-keyring, ajoutez le contenu de pamdlogin dans /etc/pam.d/login'
+#cp bash_profile ~/.bash_profile
